7606e690bedb909783811e1688432d5d39215ff2
CarmenAcosta-3 <c.a.r3@hotmail.es>
2025-07-19 19:36:56 +0100

    fix cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tu-frontend.vercel.app"],
+    origin: [/^http:\/\/localhost:\d+$/],
     credentials: true,
   })
 );
