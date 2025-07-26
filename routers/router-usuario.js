7b46b1d574ec5572043795ed524cd11a13c3f491
CarmenAcosta-3 <c.a.r3@hotmail.es>
2025-07-26 18:40:28 +0100

    fix some error sintaxis

--- routers/router-usuario.js
+++ routers/router-usuario.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/usuario.js");
 
 // Obtener todos los usuarios
 router.get("/", async (req, res) => {
